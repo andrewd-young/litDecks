1f5f19433f67ac2032e3f750953fa19a8912ca2a
Andrew Young <64940274+andrewd-young@users.noreply.github.com>
2022-07-21 15:49:44 -0400

    react and tailwind setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="mx-auto my-auto w-3/4 h-3/4">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
