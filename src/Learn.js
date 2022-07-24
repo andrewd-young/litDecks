6e976898548a9914e162c90b957f93cb1aa5c19e
Andrew Young <64940274+andrewd-young@users.noreply.github.com>
2022-07-24 17:31:33 -0400

    accepting input and parsing to arrays

--- src/Learn.js
+++ src/Learn.js
@@ -1,13 +1,12 @@
 import MultipleChoice from "./MultipleChoice";
 import Written from "./Written";
-export default function Learn() {
-  return (
-    <div className="space-y-5">
-      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <div class="bg-blue-600 h-2.5 w-45 rounded-full"></div>
-      </div>
-      <MultipleChoice></MultipleChoice>
-      <Written></Written>
-    </div>
-  );
+export default function Learn(terms) {
+	return (
+		<div className="space-y-5">
+			<div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+				<div className="bg-blue-600 h-2.5 w-45 rounded-full"></div>
+			</div>
+			<p className="text-lg dark:text-white"> {JSON.stringify(terms)} </p>
+		</div>
+	);
 }
