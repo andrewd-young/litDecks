e6bc6528ffef90cd72d76b06c746f74b47b4c04a
Andrew Young <64940274+andrewd-young@users.noreply.github.com>
2022-07-27 11:57:35 -0400

    passing counter and question

--- src/Learn.js
+++ src/Learn.js
@@ -1,18 +1,53 @@
 import MultipleChoice from "./MultipleChoice";
-import Written from "./Written";
+// import Written from "./Written";
 import {useState} from "react";
 
+let progress = 50;
+
 export default function Learn(terms) {
-  const termsArray = terms.terms;
-	let progress = 50;
+	const termsArray = terms.terms;
 	const [wordCounter, setCounter] = useState(0);
+	let questionElement = null;
+  let questionSeed = Math.random();
+
+  const setLearnCounter = () => {setCounter(wordCounter + 1)};
+
 	return (
 		<div className="space-y-5 mx-auto my-auto w-3/4 h-3/4">
 			<div className="mt-12 w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
 				<div className="bg-blue-600 h-2.5 w-45 rounded-full" style={{width: progress + "%"}}></div>
 			</div>
-			{Math.floor(Math.random() * 2) === 0 ? <Written terms={termsArray} count={wordCounter}/> : <MultipleChoice terms={termsArray} count={wordCounter}/>}
-			{/* <p className="text-lg dark:text-white"> {JSON.stringify(termsArray)} </p> */}
+			<p className="text-white">wordCount = {wordCounter}</p>
+			<Written terms={termsArray} questionSeed={questionSeed} setLearnCounter={setLearnCounter}/>
+		</div>
+	);
+}
+
+function Written(props) {
+	const termsArray = props.terms;
+	const [message, setMessage] = useState("");
+	let answerIndex = Math.floor(props.questionSeed * termsArray.length);
+
+	const handleChange = (event) => {
+		setMessage(event.target.value);
+	};
+
+	return (
+		<div className="space-y-5 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md  dark:bg-gray-800 dark:border-gray-700 h-52">
+			<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[answerIndex][1]}</label>
+			<input
+				onChange={handleChange}
+				onKeyPress={(e) => {
+					if (e.key === "Enter") {
+						props.setLearnCounter();
+					}
+				}}
+				value={message}
+				type="text"
+				id="writtenInput"
+				className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder=""
+			></input>
 		</div>
 	);
 }
--- src/MultipleChoice.js
+++ src/MultipleChoice.js
@@ -1,20 +1,19 @@
 export default function MultipleChoice(terms, count) {
-  const termsArray = terms.terms;
-  return (
-    <a className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-5">
-      <a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-        <p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
-      </a>
-      <a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-        <p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
-      </a>
-      <a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-        <p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
-      </a>
-      <a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-        <p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
-      </a>
-      <button href="" type="button" className="text-white w-auto float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Answer</button>
-    </a>
-  );
+	const termsArray = terms.terms;
+	return (
+		<a className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-5">
+			<a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+				<p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
+			</a>
+			<a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+				<p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
+			</a>
+			<a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+				<p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
+			</a>
+			<a className="block h-12 p-6 max-w-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+				<p className="text-sm font-medium text-gray-900 dark:text-gray-300">{termsArray[Math.floor(Math.random() * termsArray.length)][0]}</p>
+			</a>
+		</a>
+	);
 }
