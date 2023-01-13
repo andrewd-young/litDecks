951d8d09a848160a6aa70b9ee628e524e94362ab
Andrew Young <a.young@rivers.org>
2023-01-13 14:49:13 -0500

    text area height

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
 									value={inputData.terms}
 									onChange={handleInputChange}
 									rows="4"
-									className="block p-2.5 w-full h-44 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									className="block p-2.5 w-full h-64 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									// placeholder="Word 1&#9;&#9;Definition 1&#10;Word 2&#9;Definition 2&#10;Word 3&#9;Definition 3"
 								></textarea>
 							</div>
